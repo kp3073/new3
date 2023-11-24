0c1fb0fc8310997599300b4e4899557c211abeb2
keyurpatel <keyurpatel8288@gmail.com>
2023-11-24 15:57:13 -0500

    he

--- backend.sh
+++ backend.sh
@@ -1,27 +1,43 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+echo NODjs Disable
+dnf module disable nodejs -y >>/tmp/expense.log
 
-dnf install nodejs -y
+echo enable Nodjs
+dnf module enable nodejs:18 -y >>/tmp/expense.log
 
-cp backend.service /etc/systemd/system/backend.service
+echo installing Nodjs
+dnf install nodejs -y >>/tmp/expense.log
 
-useradd expense
+echo coping fine
+cp backend.service /etc/systemd/system/backend.service >>/tmp/expense.log
 
-mkdir /app
+echo adding useradd
+useradd expense >>/tmp/expense.log
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
+echo changing dir
+mkdir /app >>/tmp/expense.log
 
-cd /app
-npm install
 
-systemctl daemon-reload
+echo dowloading backend contant
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>/tmp/expense.log
+cd /app >>/tmp/expense.log
 
-systemctl enable backend
-systemctl start backend
+echo unziping the file
+unzip /tmp/backend.zip >>/tmp/expense.log
 
-dnf install mysql -y
+cd /app >>/tmp/expense.log
 
-mysql -h 172.31.95.103 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo installing NPm
+npm install >>/tmp/expense.log
+
+echo restarting service
+systemctl daemon-reload >>/tmp/expense.log
+
+systemctl enable backend >>/tmp/expense.log
+systemctl start backend >>/tmp/expense.log
+
+
+echo installing mysqld
+dnf install mysql -y >>/tmp/expense.log
+
+mysql -h 172.31.95.103 -uroot -pExpenseApp@1 < /app/schema/backend.sql >>/tmp/expense.log
 
