ebe6d9e6d5642ee257c45ac5787e1d7c403aabe5
keyurpatel <keyurpatel8288@gmail.com>
2023-11-26 15:17:29 -0500

    he

--- frontend.sh
+++ frontend.sh
@@ -2,13 +2,13 @@ source var.sh
 component=frontend
 
 echo installing nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_fil
 cp expense.conf /etc/nginx/default.d/expense.conf
 
 
 
 echo removing old contant
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_fil
 
 
 cd /usr/share/nginx/html
@@ -16,5 +16,5 @@ cd /usr/share/nginx/html
 download_and_extrect
 
 echo restartNginx service
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx &>>$log_fil
+systemctl restart nginx &>>$log_fil
--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,16 @@
 source var.sh
 
 echo install
-dnf module disable mysql -y >>$log_file
+dnf module disable mysql -y &>>$log_fil
 
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 echo installing Mysql
-dnf install mysql-community-server -y >>$log_file
+dnf install mysql-community-server -y &>>$log_fil
 
 echo restart service
-systemctl enable mysqld >>$log_file
-systemctl start mysqld >>$log_file
+systemctl enable mysqld &>>$log_fil
+systemctl start mysqld &>>$log_fil
 
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
