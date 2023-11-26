18ab3fea69917280028686f6ea37be1e730c9e87
keyurpatel <keyurpatel8288@gmail.com>
2023-11-26 15:23:19 -0500

    he

--- frontend.sh
+++ frontend.sh
@@ -2,19 +2,19 @@ source var.sh
 component=frontend
 
 echo installing nginx
-dnf install nginx -y &>>$log_fil
-cp expense.conf /etc/nginx/default.d/expense.conf
+dnf install nginx -y &>>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 
 
 echo removing old contant
-rm -rf /usr/share/nginx/html/* &>>$log_fil
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>$log_file
 
 download_and_extrect
 
 echo restartNginx service
-systemctl enable nginx &>>$log_fil
-systemctl restart nginx &>>$log_fil
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,16 @@
 source var.sh
 
 echo install
-dnf module disable mysql -y &>>$log_fil
+dnf module disable mysql -y &>>$log_file
 
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 echo installing Mysql
-dnf install mysql-community-server -y &>>$log_fil
+dnf install mysql-community-server -y &>>$log_file
 
 echo restart service
-systemctl enable mysqld &>>$log_fil
-systemctl start mysqld &>>$log_fil
+systemctl enable mysqld &>>$log_log_file
+systemctl start mysqld &>>$log_log_file
 
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
