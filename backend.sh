43694768dddb7b343e436009ea42f65b9c82b8c1
keyurpatel <keyurpatel8288@gmail.com>
2023-11-26 15:16:32 -0500

    he

--- backend.sh
+++ backend.sh
@@ -3,39 +3,39 @@ source var.sh
 component=backend
 
 echo NODjs Disable
-dnf module disable nodejs -y >>$log_file
+dnf module disable nodejs -y &>>$log_file
 
 echo enable Nodjs
-dnf module enable nodejs:18 -y >>$log_file
+dnf module enable nodejs:18 -y &>>$log_file
 
 echo installing Nodjs
-dnf install nodejs -y >>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo coping fine
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo adding useradd
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo changing dir
-mkdir /app >>$log_file
+mkdir /app &>>$log_file
 
-cd /app >>$log_file
+cd /app &>>$log_file
 
 download_and_extrect
 
 echo installing NPm
-npm install >>$log_file
+npm install &>>$log_file
 
 echo restarting service
-systemctl daemon-reload >>$log_file
+systemctl daemon-reload &>>$log_file
 
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 
 echo installing mysqld
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
-mysql -h 172.31.81.119 -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h 172.31.81.119 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 
