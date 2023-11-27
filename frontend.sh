ab1e1a9ca75bf8b5bfa7ebcae5f5f77208f6c7d0
keyurpatel <keyurpatel8288@gmail.com>
2023-11-27 18:05:21 -0500

    hekk

--- frontend.sh
+++ frontend.sh
@@ -3,22 +3,16 @@ compo=frontend
 
 echo installing nginx
 dnf install nginx -y &>>$log_file
+
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
- if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFaild\e[0m"
-    fi
+expression
+
 
 
 
 echo removing old contant
 rm -rf /usr/share/nginx/html/* &>>$log_file
- if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFaild\e[0m"
-    fi
+ expression
 
 
 cd /usr/share/nginx/html &>>$log_file
@@ -26,27 +20,15 @@ cd /usr/share/nginx/html &>>$log_file
 
  echo Download $compo Code
     curl -s -o /tmp/$compo.zip https://expense-artifacts.s3.amazonaws.com/$compo.zip >>$log_file
- if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFaild\e[0m"
-    fi
+ expression
 
 
     echo Extracting $compo Code
     unzip /tmp/$compo.zip >>$log_file
- if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFaild\e[0m"
-    fi
+ expression
 
 
 echo restartNginx service
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
- if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFaild\e[0m"
-    fi
+expression
--- var.sh
+++ var.sh
@@ -1,4 +1,10 @@
 log_file=/tmp/expense.log
 
 expression() {
+  if [ $? -eq 0 ]; then
+      echo -e "\e[32mSUCCESS\e[0m"
+      else
+        echo -e "\e[31mFaild\e[0m"
+      fi
+}
 
