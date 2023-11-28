9d6c89b72c7a242a61c0f7abb6d5620380ed5f2a
keyurpatel <keyurpatel8288@gmail.com>
2023-11-28 14:12:39 -0500

    hekk

--- backend.sh
+++ backend.sh
@@ -3,52 +3,41 @@ source var.sh
 
 echo NODjs Disable
 dnf module disable nodejs -y &>>$log_file
-expression
 
 echo enable Nodjs
 dnf module enable nodejs:18 -y &>>$log_file
-expression
 
 echo installing Nodjs
 dnf install nodejs -y &>>$log_file
-expression
 
 echo coping fine
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-expression
+
 
 echo adding useradd
 useradd expense &>>$log_file
-expression
 
 rn -rf /app
 
 mkdir /app
 cd /app
-expression
 
- echo Download $compo Code
-    curl -s -o /tmp/$compo.zip https://expense-artifacts.s3.amazonaws.com/$compo.zip >>$log_file
-expression
+ echo Download backend Code
+    curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
 
-    echo Extracting $compo Code
-    unzip /tmp/$compo.zip >>$log_file
-expression
+    echo Extracting backend Code
+    unzip /tmp/backend.zip >>$log_file
 cd /app
 
 echo installing NPm
 npm install &>>$log_file
-expression
 
 echo restarting service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-expression
 
 echo installing mysqld
 dnf install mysql -y &>>$log_file
-expression
 
 mysql -h 172.31.17.105 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-expression
--- frontend.sh
+++ frontend.sh
@@ -1,34 +1,28 @@
 source var.sh
-compo=frontend
 
 echo installing nginx
 dnf install nginx -y &>>$log_file
 
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-expression
 
 
 
 
 echo removing old contant
 rm -rf /usr/share/nginx/html/* &>>$log_file
- expression
 
 
 cd /usr/share/nginx/html &>>$log_file
 
 
  echo Download $compo Code
-    curl -s -o /tmp/$compo.zip https://expense-artifacts.s3.amazonaws.com/$compo.zip >>$log_file
- expression
+ curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
 
 
-    echo Extracting $compo Code
-    unzip /tmp/$compo.zip >>$log_file
- expression
+ echo Extracting frontend Code
+ unzip /tmp/frontend.zip >>$log_file
 
 
 echo restartNginx service
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-expression
