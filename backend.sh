a6c8ded36ff391ac2aa3609b43bca0a3fa0235a9
keyurpatel <keyurpatel8288@gmail.com>
2023-11-28 21:37:44 -0500

    hekk

--- backend.sh
+++ backend.sh
@@ -3,41 +3,52 @@ source var.sh
 
 echo NODjs Disable
 dnf module disable nodejs -y &>>$log_file
+expression
 
 echo enable Nodjs
 dnf module enable nodejs:18 -y &>>$log_file
+expression
 
 echo installing Nodjs
 dnf install nodejs -y &>>$log_file
+expression
 
 echo coping fine
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-
+expression
 
 echo adding useradd
 useradd expense &>>$log_file
+expression
 
 rn -rf /app
+expression
 
 mkdir /app
 cd /app
 
  echo Download backend Code
   curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
+expression
 
   echo Extracting backend Code
   unzip /tmp/backend.zip >>$log_file
 cd /app
+expression
 
 echo installing NPm
 npm install &>>$log_file
+expression
 
 echo restarting service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
+expression
 
 echo installing mysqld
 dnf install mysql -y &>>$log_file
+expression
 
 mysql -h 172.31.87.190 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+expression
