d974c890dcc16216668b8383572c2a555a273289
keyurpatel <keyurpatel8288@gmail.com>
2023-11-28 21:52:24 -0500

    hekk

--- backend.sh
+++ backend.sh
@@ -18,7 +18,10 @@ cp backend.service /etc/systemd/system/backend.service &>>$log_file
 expression
 
 echo adding useradd
-useradd expense &>>$log_file
+id expense &>>$log_file
+if [ $? -ne 0 ]; then
+ useradd expense &>>$log_file
+fi
 expression
 
 rn -rf /app
